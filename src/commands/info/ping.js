3eab3f235351eddb05f917ef5c1647d05d8b8b0f
bintangyosua <60311483+bintangyosua@users.noreply.github.com>
2021-01-06 18:28:20 +0700

    ping, serverinfo, userinfo

--- src/commands/info/ping.js
+++ src/commands/info/ping.js
@@ -1,3 +1,5 @@
+const { MessageEmbed } = require('discord.js');
+const { pong } = require('../../emojis.json');
 
 module.exports = {
     name: "ping",
@@ -6,6 +8,19 @@ module.exports = {
     aliases: ['latency'],
     usage: "ping",
     run: async (client, message, args) => {
-       message.channel.send(`Pong - ${client.ws.ping}ms`)
+        const embed = new MessageEmbed()
+            .setDescription('`Pinging...`')
+            .setColor(message.guild.me.displayHexColor);    
+        const msg = await message.channel.send(embed);
+        const timestamp = (message.editedTimestamp) ? message.editedTimestamp : message.createdTimestamp; // Check if edited
+        const latency = `\`\`\`ini\n[ ${Math.floor(msg.createdTimestamp - timestamp)}ms ]\`\`\``;
+        const apiLatency = `\`\`\`ini\n[ ${Math.round(message.client.ws.ping)}ms ]\`\`\``;
+        embed.setTitle(`Pong!  ${pong}`)
+            .setDescription('')
+            .addField('Latency', latency, true)
+            .addField('API Latency', apiLatency, true)
+            .setFooter(message.author.tag,  message.author.displayAvatarURL({ dynamic: true }))
+            .setTimestamp();
+        msg.edit(embed);
     }
 }
