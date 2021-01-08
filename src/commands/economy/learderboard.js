b09b3a1dbeb6333c9701d48f8ae391133c77008d
bintangyosua <60311483+bintangyosua@users.noreply.github.com>
2021-01-08 20:37:22 +0700

    eco

--- src/commands/economy/learderboard.js
+++ src/commands/economy/learderboard.js
@@ -1,5 +1,6 @@
+const { MessageEmbed } = require('discord.js');
 const Levels = require('discord-xp');
-const emoji = require('../../emojis.json')
+const emoji = require('../../emojis.json');
 
 module.exports = {
     name: 'leaderboard',
@@ -16,6 +17,15 @@ module.exports = {
         
         const lb = leaderboard.map(e => `${e.position}. ${e.username}#${e.discriminator}\nLevel: ${e.level}\nXP: ${e.xp.toLocaleString()}`); // We map the outputs.
         
-        message.channel.send(`**Leaderboard**:\n\n${lb.join("\n\n")}`);
+        const embed = new MessageEmbed()
+            .setAuthor('Lumina\'s Leaderboard', client.user.displayAvatarURL())
+            .setThumbnail(client.user.displayAvatarURL())
+            .setDescription(`\n\n${lb.join("\n\n")}`)
+            .setColor('RED')
+            .setTimestamp()
+            .setFooter(message.author.tag,  message.author.displayAvatarURL({ dynamic: true }))
+        
+
+        message.channel.send(embed);
     }
 }
