b42d1d14814eb0eb05177c1c566a9056414629d2
bintangyosua <60311483+bintangyosua@users.noreply.github.com>
2021-01-10 13:14:21 +0700

    durandal

--- src/commands/info/supportserver.js
+++ src/commands/info/supportserver.js
@@ -9,13 +9,13 @@ module.exports = {
     run: async (client, message, args) => {
         const embed = new MessageEmbed()
             .setTitle('Support Server')
-            .setThumbnail(client.user.displayAvatarURL())
             .setDescription('Click [here](https://discord.gg/uXFZScA) to join the Lumina Support Server!')
             .addField('Other Links', 
             '**[Invite Me](https://discord.com/oauth2/authorize?client_id=780018362138886144&scope=bot&permissions=2147483647)**'
             )
             .setFooter(message.author.tag,  message.author.displayAvatarURL({ dynamic: true }))
             .setTimestamp()
+            .setImage('https://raw.githubusercontent.com/bintangyosua/LuminaDjs/main/src/libs/images/Durandal_Banner.png?token=AOMEPO5DB7DUZXV3GNYS2U277KKU6')
             .setColor("ED80A7");
         message.channel.send(embed);
     }
