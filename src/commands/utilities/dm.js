043bcb15c7433b5be884ef0ad3c180aaaea2f8fc
bintangyosua <60311483+bintangyosua@users.noreply.github.com>
2021-01-18 18:49:54 +0700

    dm

---
+++ src/commands/utilities/dm.js
@@ -0,0 +1,26 @@
+module.exports = {
+    name: 'dm',
+    description: 'dm ',
+    category: '🕹   **Utility :**',
+    usage: "dm",
+    aliases: ["directmessage"],
+    run: async(client, message, args) => {
+
+        if (!args[0]) return message.channel.send('You need to mention a user');
+        
+        let user =
+        message.mentions.members.first() ||
+        message.guild.members.cache.get(args[0]);
+        if (!user)
+        return message.channel.send(
+          `You did not mention a user, or you gave an invalid id`
+        );
+        if (!args.slice(1).join(" "))
+        return message.channel.send("You did not specify your message");
+        user.user
+            .send(args.slice(1).join(" "))
+            .catch(() => message.channel.send("That user could not be DMed!"))
+            .then(() => message.channel.send(`Sent a message to ${user.user.tag}`));
+
+    }
+}
