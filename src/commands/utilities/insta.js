e9cfceb3edb525a531790a919c28bc683c073cbf
bintangyosua <60311483+bintangyosua@users.noreply.github.com>
2021-01-23 00:14:47 +0700

    insta

--- src/commands/utilities/insta.js
+++ src/commands/utilities/insta.js
@@ -1,5 +1,9 @@
 const Instagram = require('scraper-instagram');
 const { MessageEmbed } = require('discord.js');
+const InstagramApi = require('instagram-web-api');
+const paginationEmbed = require('discord.js-pagination');
+const { verified_instagram } = require('../../utils/emojis.json');
+const { post } = require('request-promise-native');
 
 // 3689696860%3A0RjraI9zlSn0ao%3A7
 module.exports = {
@@ -11,44 +15,34 @@ module.exports = {
     run: async(client, message, args) => {
 
         let Text = args.join(" ");
-
         if (!Text) return message.channel.send(`Please Give Something!`);
-
         let Replaced = Text.replace(/ +/g, " ");
 
         const verified = '<:instagram:800911136001228801>'
-
-        
         const InstaClient = new Instagram();
         const yourSessionId = '3689696860%3A0RjraI9zlSn0ao%3A7';
         
-
-        try {
         const instagram = await InstaClient.authBySessionId(yourSessionId)
             .catch(err => console.error(err));
         const insta = await InstaClient.getProfile(Replaced)
+        console.log(insta)
+
+        const posts = insta.lastPosts
+
+        const profileUrl = `https://www.instagram.com/${Replaced}/`
+        try {
+        
             const embed = new MessageEmbed()
-                .setAuthor(`${insta.verified ? `${Replaced} ✔` : `${Replaced}`} ${insta.private ? '🔒' : ''}`, `${insta.pic}`)
-                .setDescription(`${insta.name} \n${insta.bio}`)
+                .setTitle('\*\*Instagram\*\*')
+                .addField(`\*\*${insta.name} (@${Replaced}) ${insta.verified ? `${verified_instagram}` : ``} ${insta.private ? '🔒' : ''}\*\*`,
+`${insta.bio}
+${profileUrl}`)
                 .setColor("C32AA3")
                 .setThumbnail(insta.pic)
                 .addFields(
-                    {
-                        name: "Total Posts",
-                        value: insta.posts,
-                        inline: true
-                    },
-                    {
-                        name: "Followers",
-                        value: insta.followers,
-                        inline: true
-                    },
-                    {
-                        name: "Followings",
-                        value: insta.following,
-                        inline: true
-                
-                    },
+                    { name: "Total Posts", value: insta.posts, inline: true },
+                    { name: "Followers", value: insta.followers, inline: true },
+                    { name: "Followings", value: insta.following, inline: true },
                 )
                 .setTimestamp()
                 .setFooter(message.author.tag,  message.author.displayAvatarURL({ dynamic: true }))
