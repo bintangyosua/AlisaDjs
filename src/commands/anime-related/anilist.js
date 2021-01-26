707e70277eb266650311a7ee03b2e55d7119fab0
bintangyosua <60311483+bintangyosua@users.noreply.github.com>
2021-01-26 17:14:51 +0700

    chara

--- src/commands/anime-related/anilist.js
+++ src/commands/anime-related/anilist.js
@@ -13,7 +13,7 @@ module.exports = {
     const anilist = new Anilist.Client(ANILIST_TOKEN);
 
     let Text = args.join(" ");
-    if (!Text) return message.channel.send(`Usage: $chara <name> \n Example: $chara shirayuki`);
+    if (!Text) return message.channel.send(`Usage: $chara <name> \nExample: $chara shirayuki`);
     let Replaced = Text.replace(/ +/g, " ");
 
     try {
@@ -32,18 +32,19 @@ module.exports = {
 
         const SearchMessage = new MessageEmbed()
             .setTitle(`${title.userPreferred}`)
-            .setThumbnail(result.image.large)
             .setColor('ED80A7')
             .addFields(
                 { name: 'Name', value: `[${result.name.full} ${result.name.native}](${result.siteUrl})` },
                 { name: 'Favourites', value: `${result.favourites}` },
                 { name: 'Description', value: `${trimmedString}..` }
             )
+            .setImage(result.image.large)
             .setTimestamp()
             .setFooter(message.author.tag,  message.author.displayAvatarURL({ dynamic: true }))
         await message.channel.send(SearchMessage)
     } catch (error) {
         console.log(error)
+        message.reply('Character not found.')
     }
   }
 };
